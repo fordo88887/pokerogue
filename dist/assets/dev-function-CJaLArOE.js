6149d96ba0dde03336906264d1c1509b4e895cb7
fordo88887 <fordo88887@gmail.com>
2026-04-14 08:27:56 -0700

    Delete dist directory

--- dist/assets/dev-function-CJaLArOE.js
+++
@@ -1,2 +0,0 @@
-var t=Object.defineProperty;var n=(c,o)=>t(c,"name",{value:o,configurable:!0});async function e(){}n(e,"customDevFunction");export{e as customDevFunction};
-//# sourceMappingURL=dev-function-CJaLArOE.js.map
